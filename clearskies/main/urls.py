0fc1931cd4da05c84724ef1144c2bb249dc06b77
kunsergio117 <kunsergio117@outlook.com>
2024-05-01 16:55:00 +0800

    removed login capabilities in light of easier use, besides it is meant for public use

--- clearskies/main/urls.py
+++ clearskies/main/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views  # Import the views for authentication
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('admin/', admin.site.urls),
 ]
--- clearskies/main/views.py
+++ clearskies/main/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render
 import json
 import urllib.request
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
+
+import json
+import urllib.request
+from django.shortcuts import render
 
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-
-        # Remove spaces from the URL and properly encode it
         url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=638f32faed524313d58727ce950d0e20'
-        # Encode the URL to handle special characters
         url = urllib.parse.quote(url, safe=':/?&=')
-
-        # Make the request to the API
         source = urllib.request.urlopen(url).read()
-
-        # Convert JSON data to a dictionary
         list_of_data = json.loads(source)
-
-        # Extract required data
         data = {
             "country_code": str(list_of_data['sys']['country']),
             "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
@@ -30,3 +26,6 @@ def index(request):
     else:
         data = {}
     return render(request, "main/index.html", data)
+
+
+
